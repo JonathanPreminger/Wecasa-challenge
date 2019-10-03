5fd8718828df1466f55bcfc575a56bc35f3e02e1
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-10-03 21:10:28 +0200

    create a module Matching for keeping the controller dry

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,11 @@
 class BookingsController < ApplicationController
+  include Matching
   require 'set'
   require "time"
 
   def create
     @booking = Booking.create!(booking_params)
-
-     redirect_to bookings_path
-
+    redirect_to bookings_path
   end
 
   def new
@@ -14,86 +13,12 @@ class BookingsController < ApplicationController
   end
 
   def show
-    @pros = Pro.all
     @booking = Booking.find(params[:id])
-    @skills_required = []
-    @booking.prestations.each do |presta|
-      @skills_required << presta.reference
-    end
-    who_got_the_skills
-    match_datetime
-  end
-
-  def who_got_the_skills
-    @skills_availlable = []
-    @pro_valid = []
-    @pros.each do |pro|
-      @name = pro.name
-      @skills_availlable.clear
-      pro.prestations.each do |presta_availlable|
-        @skills_availlable << presta_availlable.reference
-      end
-      @y = 0
-      @pi = @skills_required.size
-      @skills_required.each do |x|
-        if @skills_availlable.include?(x)
-          @y = @y + 1
-        else
-          @y= @y - 1
-        end
-      end
-      if @y ==  @pi
-          @pro_valid << pro.name
-        end
-    end
-  end
-
-  def match_datetime
-    match_day
-    match_hour
-  end
-
-  def match_day
-    @get_pro_back_for_day_tchek = []
-    @pro_day_available = []
-    @pro_valid.each do |p|
-      @get_pro_back_for_day_tchek << Pro.find_by(name:p)
-    end
-    @get_pro_back_for_day_tchek.each do |g|
-      g.openning_hours.each do |a|
-        if a.day == @booking.starts_at.strftime("%A").downcase
-          @pro_day_available << g.name
-        end
-      end
-    end
-  end
-
-  def match_hour
-    @get_pro_back_for_hour_tchek = []
-    @pro_hour_available = []
-    @pro_day_available.each do |p|
-      @get_pro_back_for_hour_tchek << Pro.find_by(name:p)
-    end
-    @get_pro_back_for_hour_tchek.each do |g|
-      g.openning_hours.each do |a|
-        @t =  @booking.starts_at + 2*60*60
-        if  @t.strftime("%I:%M%p").between?(a.starts_at.strftime("%I:%M%p"), a.ends_at.strftime("%I:%M%p"))
-          @pro_hour_available << g.name
-        end
-      end
-    end
-  end
-
-  def edit
-    @booking = Booking.find(params[:id])
+    matching_all
   end
 
   def index
     @bookings  = Booking.all
-  end
-
-  def destroy
-    @booking = Booking.find(params[:id])
   end
 
   private
---
+++ app/controllers/concerns/matching.rb
@@ -0,0 +1,81 @@
+module Matching
+  extend ActiveSupport::Concern
+
+  def matching_all
+    skills_required
+    who_got_the_skills
+    match_datetime
+  end
+
+  def skills_required
+    @pros = Pro.all
+    @skills_required = []
+    @booking.prestations.each do |presta|
+      @skills_required << presta.reference
+    end
+  end
+
+  def who_got_the_skills
+    @skills_availlable = []
+    @pro_valid = []
+    @pros.each do |pro|
+      @name = pro.name
+      @skills_availlable.clear
+      pro.prestations.each do |presta_availlable|
+        @skills_availlable << presta_availlable.reference
+      end
+      @y = 0
+      @pi = @skills_required.size
+      @skills_required.each do |x|
+        if @skills_availlable.include?(x)
+          @y = @y + 1
+        else
+          @y= @y - 1
+        end
+      end
+      if @y ==  @pi
+          @pro_valid << pro.name
+        end
+    end
+  end
+
+  def match_datetime
+    match_day
+    match_hour
+  end
+
+  def match_day
+    @get_pro_back_for_day_tchek = []
+    @pro_day_available = []
+    @pro_valid.each do |p|
+      @get_pro_back_for_day_tchek << Pro.find_by(name:p)
+    end
+    @get_pro_back_for_day_tchek.each do |g|
+      g.openning_hours.each do |a|
+        if a.day == @booking.starts_at.strftime("%A").downcase
+          @pro_day_available << g.name
+        end
+      end
+    end
+  end
+
+  def match_hour
+    @get_pro_back_for_hour_tchek = []
+    @pro_hour_available = []
+    @pro_day_available.each do |p|
+      @get_pro_back_for_hour_tchek << Pro.find_by(name:p)
+    end
+    @get_pro_back_for_hour_tchek.each do |g|
+      g.openning_hours.each do |a|
+        @t =  @booking.starts_at + 2*60*60
+        @u= @t.strftime("%B %d, %Y %H:%M'")
+        puts" ____________________________________________  "
+        puts @t
+        puts @u
+        if  @t.strftime("%I:%M%p").between?(a.starts_at.strftime("%I:%M%p"), a.ends_at.strftime("%I:%M%p"))
+          @pro_hour_available << g.name
+        end
+      end
+    end
+  end
+end
