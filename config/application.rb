084c8750d7d4d3a2001b28ad00a5e90c6fba87fc
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-10-04 16:10:20 +0200

    add font, and style show booking

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ module WecasaChallenge
     config.load_defaults 5.2
     config.time_zone = 'Europe/Paris'
     config.active_record.default_timezone = :local
+    config.assets.paths << Rails.root.join("app", "assets", "fonts..")
+  config.assets.paths << Rails.root.join("app/assets/fonts")
+  config.assets.precompile += %w(.png)
+  config.assets.enabled = true
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
